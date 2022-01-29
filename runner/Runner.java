350ae16b3a018a9e82d8f545bc356a3082a24de6
Ahalyask <37923115+Ahalyask@users.noreply.github.com>
2022-01-30 00:06:27 +0530

    week6day1

---
+++ runner/Runner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.CucumberOptions.SnippetType;
+
+
+@CucumberOptions(features="src/main/java/feature/CreateLead.feature"
+				, glue="steps"
+				, monochrome = true
+				, dryRun = false
+				, snippets = SnippetType.CAMELCASE
+				,publish = true)
+public class Runner extends AbstractTestNGCucumberTests{
+
+}
